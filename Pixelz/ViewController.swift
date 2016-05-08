935e3cd9e9433a3e3fd5635c5df4e91cd77b9179
GaryRudolph <gary@lolay.com>
2016-05-08 09:36:54 -0700

    Added the buttons to the screen that are 64x64 in size

--- Pixelz/ViewController.swift
+++ Pixelz/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		let buttonHeight = 64.0
+		let buttonWidth = 64.0
+		let numberOfRows = Int(floor(Double(view.bounds.height) / buttonHeight))
+		let numberOfColumns = Int(floor(Double(view.bounds.width) / buttonWidth))
+		
+		for row in 0..<numberOfRows {
+			for column in 0..<numberOfColumns {
+				let button = UIButton(frame: CGRect(x: Double(column) * buttonWidth, y: Double(row) * buttonHeight, width: buttonWidth, height: buttonHeight))
+				button.backgroundColor = UIColor.blueColor()
+				button.setTitle("\(column):\(row)", forState: UIControlState.Normal)
+				self.view.addSubview(button)
+			}
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
