77fb25ca803a1c7da105623f0be97d86d8c71e87
Connor Rudolph <connor@moonracer.local>
2016-05-21 16:45:24 -0700

    added button handling

--- Pixelz/ViewController.swift
+++ Pixelz/ViewController.swift
@@ -21,11 +21,18 @@ class ViewController: UIViewController {
 			for column in 0..<numberOfColumns {
 				let button = UIButton(frame: CGRect(x: Double(column) * buttonWidth, y: Double(row) * buttonHeight, width: buttonWidth, height: buttonHeight))
 				button.backgroundColor = UIColor.blueColor()
-				button.setTitle("\(column):\(row)", forState: UIControlState.Normal)
+                button.tag = (column * 100) + row
+				button.setTitle("\(button.tag)", forState: UIControlState.Normal)
+                button.addTarget(self, action: #selector(ViewController.didReceiveButtonPress(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+
 				self.view.addSubview(button)
-			}
+            }
 		}
 	}
+    
+    @IBAction func didReceiveButtonPress(sender: UIButton) {
+         print("didRecieveButtonPress \(sender.tag)")
+    }
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
@@ -34,4 +41,3 @@ class ViewController: UIViewController {
 
 
 }
-
